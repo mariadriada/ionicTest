572858e9e8bf25b303b3bfa28e1674c6b204de15
Maria Eugenia Giraldo Forero <megiraldo@CORANTIOQUIA.LOCAL>
2016-09-26 16:25:26 -0500

    Create Files to try menu

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,42 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  //template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
+
+export /**
+ * MyApp
+ */
+class  MyApp {
+  
+  private root: any; 
+
+  pages: Array<{title: string, component:any}>
+
+  constructor(
+    private platform: Platform,
+    private menu: MenuController
+  ) {
+    this.initializeApp();
+    
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      StatusBar.styleDefault();
+    });
+  }
+
+}
+
+
+
+/*
 export class MyApp {
 
   private rootPage: any;
@@ -21,5 +51,6 @@ export class MyApp {
     });
   }
 }
+*/
 
 ionicBootstrap(MyApp);
